90c5cdf6f47969b5df388baf2823e59284093fd1
ManuelaFrancoOquendo <manuelafranco1502@gmail.com>
2024-05-19 02:03:34 -0500

    creation part of the tasks table.

---
+++ TaskManagement/TaskManagement/DAL/Entities/AudiBase.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagement.DAL.Entities
+{
+    public class AudiBase
+    {
+        [Key]
+        [Required]
+        public virtual Guid Id { get; set; } // Esta sera el PK de todas las tablas, siemore. son los IDS largos, altamente seguro, alfanumericos 
+        
+        public virtual DateTime? CreatedDate { get; set; } // para guardar nuevo con su fecha de creacion.
+        
+        public virtual DateTime? ModifiedDate { get; set; } // para guardar todo registro que se modifico con su fecha de modificacion.
+
+    }
+}
---
+++ TaskManagement/TaskManagement/DAL/Entities/Tasks.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagement.DAL.Entities
+{
+    public class Tasks : AudiBase
+    {
+        [Display(Name = "Título")] //Para identificar el nombre mucho mas facil.
+        [MaxLength(50, ErrorMessage ="El campo {0} debe tener máximo {1} caracteres.")] //Longitud maxima
+        [Required(ErrorMessage = "Es campo {0} es obligatorio")] //Para que sea requerido
+        public string Title { get; set; }
+
+        [Display(Name = "Descripción de la tarea")] //Para identificar el nombre mucho mas facil.
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")] //Longitud maxima
+        public string Description { get; set; }
+
+
+        //Este atributo no se ingresa.
+        [Display(Name = "Tarea completada")] //Para identificar el nombre mucho mas facil.
+        [MaxLength(30, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")] //Longitud maxima
+        [Required(ErrorMessage = "Es campo {0} es obligatorio")] //Para que sea requerido
+        public string IsCompleted { get; set; }
+
+
+        [Display(Name = "Prioridad de la tarea")] //Para identificar el nombre mucho mas facil.
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")] //Longitud maxima
+        [Required(ErrorMessage = "Es campo {0} es obligatorio")] //Para que sea requerido
+        public string Priority { get; set; }
+
+
+        //Las siguientes entidades no se si ponerlas ya que pueden ser parte de la auditoria.
+    //    public DateTime DueDate { get; set; }
+    //    public DateTime CreateDate { get; set; }
+    //    public DateTime CompletionDate { get; set; }
+    //}
+}
+    
